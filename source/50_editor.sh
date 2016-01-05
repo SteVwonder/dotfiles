51153e7651d998bf23260ee2f2e87ab9cea35af1
Stephen Herbein <stephen272@gmail.com>
2016-01-04 19:47:33 -0800

    editor: fixed issues with ALTERNATE_EDITOR
    is it is set to the empty string, then emacsclient will automatically
    start the server and try again

--- source/50_editor.sh
+++ source/50_editor.sh
@@ -1,19 +1,13 @@
 # Editing
 
-if [[ ! "$SSH_TTY" ]] && is_osx; then
-  export EDITOR='mvim'
-  export LESSEDIT='mvim ?lm+%lm -- %f'
-else
-  export EDITOR='vim'
-fi
-
 if [[ "$(type -P emacsclient)" ]]; then
     EDITOR="emacsclient"
 else
     EDITOR="emacs"
 fi
+
 export EDITOR
 export SUDO_EDITOR=$EDITOR
-
+export ALTERNATE_EDITOR=""
 export VISUAL="$EDITOR"
 alias q="$EDITOR"
