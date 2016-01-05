bc1f1cd9d497c25b9c6d3987462c3f8413fa104e
Stephen Herbein <stephen272@gmail.com>
2016-01-05 01:36:05 -0500

    ubuntu: fixed volnoti and emacs compilation

--- init/20_ubuntu_apt.sh
+++ init/20_ubuntu_apt.sh
@@ -8,6 +8,7 @@ sudo apt-get -qq dist-upgrade
 
 # Install APT packages.
 packages=(
+  autoconf
   build-essential
   emacs24
   feh
@@ -15,6 +16,9 @@ packages=(
   htop
   id3tool
   i3
+  libdbus-glib-1-dev
+  libgtk2.0-dev
+  libncurses-dev
   libssl-dev
   tmux
   tree
@@ -41,13 +45,26 @@ if [[ ! "$(type -P git-extras)" ]]; then
 fi
 
 # Install Volnoti
-if [[ ! "$(type -P git-extras)" ]]; then
+if [[ ! "$(type -P volnoti)" ]]; then
   e_header "Installing Volnoti"
   (
     cd $DOTFILES/vendor/volnoti &&
-    bash prepare.sh &&
-    ./configure --prefix=/usr/local &&
-    make &&
-    sudo make install
+        bash prepare.sh &&
+        bash fix_headers.sh &&
+        ./configure --prefix=/usr/local &&
+        make &&
+        sudo make install
   )
 fi
+
+# Install Emacs
+e_header "Installing Emacs"
+(
+    cd $DOTFILES/vendor/ &&
+        wget http://mirror.lug.udel.edu/pub/gnu/emacs/emacs-24.5.tar.xz &&
+        tar -xf ./emacs-24.5.tar.xz &&
+        cd emacs-24.5 &&
+        ./configure --prefix=/usr/local --without-x &&
+        make -j4 &&
+        sudo make install
+)
