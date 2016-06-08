a819b62166eb3bc4969e6e7fe517fd84f4a4566f
Stephen Herbein <stephen272@gmail.com>
2016-06-08 16:09:37 -0700

    Store separate history for each term under bash
    Histories are categorizes by year/month/day
    
    Fixes #5

--- source/50_history.sh
+++ source/50_history.sh
@@ -8,11 +8,22 @@ shopt -s histverify
 # Entries beginning with space aren't added into history, and duplicate
 # entries will be erased (leaving the most recent entry).
 export HISTCONTROL="ignorespace:erasedups"
+# Common commands are ignored
+export HISTIGNORE='ls:bg:fg:history'
 # Give history timestamps.
 export HISTTIMEFORMAT="[%F %T] "
 # Lots o' history.
 export HISTSIZE=10000
-export HISTFILESIZE=10000
+export HISTFILESIZE=1000000
+
+# Hist file for each terminal/day
+TODAYS_DIR=${HOME}/.history/$(date "+%Y")/$(date "+%m")/$(date "+%d")
+mkdir -p ${TODAYS_DIR}
+MYTTY=$(basename `tty`)
+export HISTFILE=${TODAYS_DIR}/"$(date "+%H-%M-%S")_${MYTTY}.hist"
+
+# record each command as it is executed, rather than at the end
+export PROMPT_COMMAND='history -a'
 
 # Easily re-execute the last history command.
 # alias r="fc -s"
