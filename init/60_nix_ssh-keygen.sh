612c12a57b5f8e47e9d55e68a4b341a85c6c2c33
Stephen Herbein <stephen272@gmail.com>
2016-06-09 22:21:18 -0700

    Add printing for ssh-keygen init script

--- init/60_nix_ssh-keygen.sh
+++ init/60_nix_ssh-keygen.sh
@@ -6,6 +6,9 @@ mkdir -p ${key_directory}
 for key_title in ${key_titles[@]}; do
     key_file="${key_directory}/${key_title}"
     if [ ! -f ${key_file} ]; then
+        echo "Creating ${key_file}"
         ssh-keygen -t rsa -b 4096 -f ${key_file} -N "" -q
+    else
+        echo "Skipping ${key_file}, already exists"
     fi
 done
