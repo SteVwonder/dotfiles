c190891fcede8a31251077e8c52ca48ac5af493b
Stephen Herbein <stephen272@gmail.com>
2016-07-03 11:52:44 -0700

    Prevent errors running under zsh

--- link/.bashrc
+++ link/.bashrc
@@ -11,8 +11,8 @@ function src() {
   if [[ "$1" ]]; then
     source "$DOTFILES/source/$1.sh"
   else
-    for file in $DOTFILES/source/*; do
-      source "$file"
+      for file in $DOTFILES/source/*; do
+          source "$file"
     done
   fi
 }
--- source/50_misc.sh
+++ source/50_misc.sh
@@ -1,9 +1,27 @@
-# Case-insensitive globbing (used in pathname expansion)
-shopt -s nocaseglob
+if [ ! -z "$BASH" ]; then
+    # Case-insensitive globbing (used in pathname expansion)
+    shopt -s nocaseglob
 
-# Check the window size after each command and, if necessary,
-# update the values of LINES and COLUMNS.
-shopt -s checkwinsize
+    # Check the window size after each command and, if necessary,
+    # update the values of LINES and COLUMNS.
+    shopt -s checkwinsize
+
+    # SSH auto-completion based on entries in known_hosts.
+    if [[ -e ~/.ssh/known_hosts ]]; then
+        complete -o default -W "$(cat ~/.ssh/known_hosts | sed 's/[, ].*//' | sort | uniq | grep -v '[0-9]')" ssh scp sftp
+    fi
+
+    # Prevent ctrl+s from freezing the terminal
+    # For more info: http://superuser.com/questions/124845/can-you-disable-the-ctrl-s-xoff-keystroke-in-putty
+    if [ ! -z "$PS1" ]; then
+        stty -ixon
+    fi
+
+    # SSH auto-completion based on entries in known_hosts.
+    if [[ -e ~/.ssh/known_hosts ]]; then
+        complete -o default -W "$(cat ~/.ssh/known_hosts | sed 's/[, ].*//' | sort | uniq | grep -v '[0-9]')" ssh scp sftp
+    fi
+fi
 
 alias grep="grep --color=auto"
 
@@ -14,14 +32,3 @@ export LESS=-XR
 function titlebar() {
   echo -n $'\e]0;'"$*"$'\a'
 }
-
-# Prevent ctrl+s from freezing the terminal
-# For more info: http://superuser.com/questions/124845/can-you-disable-the-ctrl-s-xoff-keystroke-in-putty
-if [ ! -z "$PS1" ]; then
-    stty -ixon
-fi
-
-# SSH auto-completion based on entries in known_hosts.
-if [[ -e ~/.ssh/known_hosts ]]; then
-  complete -o default -W "$(cat ~/.ssh/known_hosts | sed 's/[, ].*//' | sort | uniq | grep -v '[0-9]')" ssh scp sftp
-fi
