14a87f5a8db0e43121258a6f2a3f18c4d4381c8a
Stephen Herbein <stephen272@gmail.com>
2016-07-05 09:11:46 -0700

    Disable ctrl+s from stopping terminal/shell

--- source/50_misc.sh
+++ source/50_misc.sh
@@ -15,6 +15,12 @@ function titlebar() {
   echo -n $'\e]0;'"$*"$'\a'
 }
 
+# Prevent ctrl+s from freezing the terminal
+# For more info: http://superuser.com/questions/124845/can-you-disable-the-ctrl-s-xoff-keystroke-in-putty
+if [ ! -z "$PS1" ]; then
+    stty -ixon
+fi
+
 # SSH auto-completion based on entries in known_hosts.
 if [[ -e ~/.ssh/known_hosts ]]; then
   complete -o default -W "$(cat ~/.ssh/known_hosts | sed 's/[, ].*//' | sort | uniq | grep -v '[0-9]')" ssh scp sftp
