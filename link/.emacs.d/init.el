3e1f3102a3e93c4d3e228c152d66a45237d2eb0f
Stephen Herbein <stephen272@gmail.com>
2016-07-06 17:58:07 -0700

    Swap org mode outline colors
    The dark blue used for top level outline points was always hard for me
    to read so this swaps it with a different, but still "bold" color

--- link/.emacs.d/init.el
+++ link/.emacs.d/init.el
@@ -60,6 +60,7 @@
   (setq w3m-user-agent "Mozilla/5.0 (Linux; U; Android 2.3.3; zh-tw; HTC_Pyramid Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.")
 )
 
+(require 'setup-org)
 (require 'setup-company)
 (require 'setup-helm)
 (require 'setup-tex)
---
+++ link/.emacs.d/site-lisp/setup-org.el
@@ -0,0 +1,7 @@
+(require 'org)
+
+(set-face-attribute 'org-level-1 nil :inherit 'outline-3)
+(set-face-attribute 'org-level-3 nil :inherit 'outline-6)
+(set-face-attribute 'org-level-6 nil :inherit 'outline-1)
+
+(provide 'setup-org)
