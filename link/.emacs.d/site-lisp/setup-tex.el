72bdca6da3ed1e356e0b4f6d4ca7addf42d6be88
Stephen Herbein <stephen272@gmail.com>
2017-04-28 12:08:21 -0400

    Add sync for okular and emacs

--- link/.emacs.d/site-lisp/setup-tex.el
+++ link/.emacs.d/site-lisp/setup-tex.el
@@ -3,7 +3,20 @@
   :init
   (setq TeX-PDF-mode t)
   (add-hook 'LaTeX-mode-hook 'visual-line-mode)
-    )
+  ;; ##### Always ask for the master file
+  ;; ##### when creating a new TeX file.
+  (setq-default TeX-master nil)
+
+
+  ;; ##### Enable synctex correlation. From Okular just press
+  ;; ##### Shift + Left click to go to the good line.
+  (setq TeX-source-correlate-mode t
+        TeX-source-correlate-start-server t)
+
+  ;; ### Set Okular as the default PDF viewer.
+  (eval-after-load "tex"
+    '(setcar (cdr (assoc 'output-pdf TeX-view-program-selection)) "Okular"))
+  )
 
 (use-package auctex-latexmk
   :ensure t
@@ -14,5 +27,12 @@
 (use-package company-auctex
   :ensure t
   )
+
+;; ##### Don't forget to configure
+;; ##### Okular to use emacs in
+;; ##### "Configuration/Configure Okular/Editor"
+;; ##### => Editor => Emacsclient. (you should see
+;; ##### emacsclient -a emacs --no-wait +%l %f
+;; ##### in the field "Command".
 
 (provide 'setup-tex)
