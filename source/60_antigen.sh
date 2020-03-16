63175416b43379a1919a984ed8611b0733d0d557
Stephen Herbein <stephen272@gmail.com>
2020-03-15 18:12:11 -0700

    antigen: switch to here-doc for bundle list

--- source/60_antigen.sh
+++ source/60_antigen.sh
@@ -6,11 +6,13 @@ source $DOTFILES/vendor/antigen/bin/antigen.zsh
 antigen use oh-my-zsh
 
 # Bundles from the default repo (robbyrussell's oh-my-zsh).
-antigen bundle git
-antigen bundle command-not-found
+antigen bundle <<EOBUNDLES
+ git
+ command-not-found
 
-# Syntax highlighting bundle.
-antigen bundle zsh-users/zsh-syntax-highlighting
+ # Syntax highlighting bundle.
+ zsh-users/zsh-syntax-highlighting
+EOBUNDLES
 
 # Load the theme.
 antigen bundle agkozak/agkozak-zsh-prompt
