1a20f90f91fbf195c158575460770e6def45f1f9
Stephen Herbein <stephen272@gmail.com>
2022-02-12 06:43:45 -0800

    rust: add lsp mode

--- link/.emacs.d/site-lisp/setup-coding-modes.el
+++ link/.emacs.d/site-lisp/setup-coding-modes.el
@@ -69,4 +69,47 @@
   :init (add-to-list 'auto-mode-alist (cons "\\.adoc\\'" 'adoc-mode))
   )
 
+;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l", "s-l")
+(setq lsp-keymap-prefix "C-c l")
+
+(use-package lsp-mode
+  :ensure t
+  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+         ;; (XXX-mode . lsp)
+         (rust-mode . lsp)
+         ;; if you want which-key integration
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
+
+;; optionally
+(use-package lsp-ui
+  :ensure t
+  :commands lsp-ui-mode)
+;; if you are helm user
+(use-package helm-lsp
+  :ensure t
+  :commands helm-lsp-workspace-symbol)
+;; if you are ivy user
+;; (use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
+;; (use-package lsp-treemacs :commands lsp-treemacs-errors-list)
+
+;; optionally if you want to use debugger
+;; (use-package dap-mode)
+;; (use-package dap-LANGUAGE) to load the dap adapter for your language
+
+;; optional if you want which-key integration
+(use-package which-key
+  :ensure t
+  :config
+  (which-key-mode))
+
+;; The default setting is too low for lsp-mode’s needs due to the fact that
+;; client/server communication generates a lot of memory/garbage.
+(setq gc-cons-threshold 100000000)
+
+;; Increase the amount of data which Emacs reads from the process. Again the
+;; emacs default is too low 4k considering that the some of the language server
+;; responses are in 800k - 3M range.
+(setq read-process-output-max (* 1024 1024)) ;; 1mb
+
 (provide 'setup-coding-modes)
