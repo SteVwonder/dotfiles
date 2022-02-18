43fb413d8da302e371f38103042fa5d9cff06bfd
Stephen Herbein <SteVwonder@users.noreply.github.com>
2022-02-18 14:22:50 -0800

    Update 30_osx_homebrew_recipes.sh
    Remove some old packages and add zsh

--- init/30_osx_homebrew_recipes.sh
+++ init/30_osx_homebrew_recipes.sh
@@ -8,20 +8,18 @@ is_osx || return 1
 recipes=(
   autoconf
   automake
-  bash
   emacs
   gcc
   git
   git-extras
-  htop-osx
   reattach-to-user-namespace
   ssh-copy-id
   sshfs
   subversion
   tmux
   tree
-  w3m
   wget
+  zsh
 )
 
 brew_install_recipes
@@ -31,20 +29,13 @@ brew_install_recipes
 # This is where brew stores its binary symlinks
 local binroot="$(brew --config | awk '/HOMEBREW_PREFIX/ {print $2}')"/bin
 
-# htop
-if [[ "$(type -P $binroot/htop)" ]] && [[ "$(stat -L -f "%Su:%Sg" "$binroot/htop")" != "root:wheel" || ! "$(($(stat -L -f "%DMp" "$binroot/htop") & 4))" ]]; then
-  e_header "Updating htop permissions"
-  sudo chown root:wheel "$binroot/htop"
-  sudo chmod u+s "$binroot/htop"
+# zsh
+if [[ "$(type -P $binroot/zsh)" && "$(cat /etc/shells | grep -q "$binroot/zsh")" ]]; then
+  e_header "Adding $binroot/zsh to the list of acceptable shells"
+  echo "$binroot/zsh" | sudo tee -a /etc/shells >/dev/null
 fi
-
-# bash
-if [[ "$(type -P $binroot/bash)" && "$(cat /etc/shells | grep -q "$binroot/bash")" ]]; then
-  e_header "Adding $binroot/bash to the list of acceptable shells"
-  echo "$binroot/bash" | sudo tee -a /etc/shells >/dev/null
-fi
-if [[ "$(dscl . -read ~ UserShell | awk '{print $2}')" != "$binroot/bash" ]]; then
-  e_header "Making $binroot/bash your default shell"
-  sudo chsh -s "$binroot/bash" "$USER" >/dev/null 2>&1
+if [[ "$(dscl . -read ~ UserShell | awk '{print $2}')" != "$binroot/zsh" ]]; then
+  e_header "Making $binroot/zsh your default shell"
+  sudo chsh -s "$binroot/zsh" "$USER" >/dev/null 2>&1
   e_arrow "Please exit and restart all your shells."
 fi
