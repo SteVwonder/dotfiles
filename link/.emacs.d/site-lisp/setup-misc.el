314b8bec0be17ad79c6ba7a187216849a01c5635
Stephen Herbein <sherbein@nvidia.com>
2022-04-14 13:00:54 -0600

    emacs: fix annoying undo-tree files being created in-tree

--- link/.emacs.d/site-lisp/setup-misc.el
+++ link/.emacs.d/site-lisp/setup-misc.el
@@ -18,6 +18,8 @@
 (use-package undo-tree
   :ensure t
   :config (global-undo-tree-mode)
+  ;; Prevent undo tree files from polluting your git repo
+  (setq undo-tree-history-directory-alist '(("." . "~/.emacs.d/undo")))
   )
 
 (use-package vlf
