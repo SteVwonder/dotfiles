70b13e76e8c5f91d81951a7d5ee934198948fa9b
Stephen Herbein <stephen@herbein.net>
2023-01-21 16:27:40 -0500

    [i3] drop dmenu-frecency

--- link/.i3/dmenu-desktop.sh
+++ link/.i3/dmenu-desktop.sh
@@ -2,9 +2,10 @@
 
 PATH=$HOME/.dotfiles/bin:$PATH
 
-if command -v dmenu-frecency; then
-    exec dmenu-frecency
-elif command -v j4-dmenu-desktop && command -v rofi; then
+#if command -v dmenu-frecency; then
+    #exec dmenu-frecency
+#el
+if command -v j4-dmenu-desktop && command -v rofi; then
     exec j4-dmenu-desktop --usage-log=~/.cache/j4-usage.log --dmenu='rofi -dmenu -i'
 elif command -v j4-dmenu-desktop; then
     exec j4-dmenu-desktop --usage-log=~/.cache/j4-usage.log
