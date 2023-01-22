1f0c7d2f19d1dcbb183c63f333ed715bae19d6fb
Stephen Herbein <sherbein@nvidia.com>
2023-01-21 19:48:48 -0500

    [emacs] tweak magit for speed

--- link/.emacs.d/site-lisp/setup-misc.el
+++ link/.emacs.d/site-lisp/setup-misc.el
@@ -12,6 +12,11 @@
                     "^Enter passphrase for .*: ?$"))
     (add-to-list 'magit-process-password-prompt-regexps regexp))
   (add-hook 'git-commit-mode-hook #'setup-commit-mode)
+  (remove-hook 'magit-status-sections-hook 'magit-insert-tags-header)
+  (remove-hook 'magit-status-sections-hook 'magit-insert-status-headers)
+  :custom
+  ;; Supposedly this help performance?
+  (magit-git-executable "/usr/local/bin/git")
   )
 
 (use-package undo-tree
