0146b39da976bc2cd27f05134fc00ec9adb8b7e5
Stephen Herbein <stephen@herbein.net>
2023-08-20 12:51:30 -0400

    [fish] add fisher and some plugins

--- conf/fish/dotfiles.fish
+++ conf/fish/dotfiles.fish
@@ -1,4 +1,13 @@
 #!/usr/bin/fish
 if status is-interactive
    starship init fish | source
+   if ! functions --query fisher
+      test -z "$XDG_CONFIG_HOME" && set -f XDG_CONFIG_HOME $HOME/.config
+      set -f fisherFile $XDG_CONFIG_HOME/fish/functions/fisher.fish
+      if ! test -e "$fisherFile"
+         echo "FILE DOESNT EXIST"
+         curl --create-dirs -sLo "$fisherFile" https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish
+      end
+      source $fisherFile && fisher install jorgebucaran/fisher
+   end
 end
--- init/40_fish.sh
+++ init/40_fish.sh
@@ -1,4 +1,6 @@
-FISH_CONFIG_DIRECTORY="$HOME/.config/fish/conf.d/"
+FISH_CONFIG_DIRECTORY="$HOME/.config/fish/"
+FISH_CONFD_DIRECTORY="$FISH_CONFIG_DIRECTORY/conf.d/"
 
-mkdir -p "$FISH_CONFIG_DIRECTORY"
-ln -sf $DOTFILES/conf/fish/dotfiles.fish "$FISH_CONFIG_DIRECTORY/"
+mkdir -p "$FISH_CONFD_DIRECTORY"
+ln -sf $DOTFILES/conf/fish/fish_plugins "$FISH_CONFIG_DIRECTORY/"
+ln -sf $DOTFILES/conf/fish/dotfiles.fish "$FISH_CONFD_DIRECTORY/"
