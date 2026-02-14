c169fcf18c5390f2e084d62cb09b74fd03155348
Stephen Herbein <sherbein@nvidia.com>
2026-02-14 16:11:42 -0500

    [emcs] suppress cl deprecated message

--- link/.emacs.d/early-init.el
+++ link/.emacs.d/early-init.el
@@ -1,1 +1,4 @@
 (setq package-enable-at-startup nil)
+;; Suppress "package cl is deprecated" warnings from third-party packages
+;; that still use cl instead of cl-lib
+(setq byte-compile-warnings '(not cl-functions obsolete))
