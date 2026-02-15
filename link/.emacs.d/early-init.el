664b03d8e90605d8296b826ae740826c3b94dd2b
Stephen Herbein <sherbein@nvidia.com>
2026-02-14 20:46:43 -0500

    [emacs] disable the toolbar in graphical emacs

--- link/.emacs.d/early-init.el
+++ link/.emacs.d/early-init.el
@@ -1,4 +1,5 @@
 (setq package-enable-at-startup nil)
+(tool-bar-mode -1)
 ;; Suppress "package cl is deprecated" warnings from third-party packages
 ;; that still use cl instead of cl-lib
 (setq byte-compile-warnings '(not cl-functions obsolete))
