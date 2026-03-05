8835b90494644b58cb75d147d4f0979afced16c2
Stephen Herbein <sherbein@nvidia.com>
2026-03-05 03:25:35 -0800

    [editor] use vscode-direct terminfo if available
    That terminfo works well under mosh, unlike xterm-emacs
    
    Inspiration: https://chadaustin.me/2024/01/truecolor-terminal-emacs/

--- source/50_editor.sh
+++ source/50_editor.sh
@@ -1,12 +1,18 @@
 # Editing
 
+if [[ -f /usr/share/terminfo/v/vscode-direct ]]; then
+    TERM="vscode-direct"
+else
+    TERM="xterm-emacs"
+fi
+
 if type emacsclient &> /dev/null; then
     EDITOR="emacsclient"
-    alias emacsclient="TERM=xterm-emacs emacsclient"
+    alias emacsclient="TERM=$TERM emacsclient"
     alias emacs="emacsclient -c"
 else
     EDITOR="emacs"
-    alias emacs="TERM=xterm-emacs emacs"
+    alias emacs="TERM=$TERM emacs"
 fi
 
 export EDITOR="${EDITOR}"
