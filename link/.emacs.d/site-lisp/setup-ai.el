50dd54d0cd26a85f3f2c9636194a8eef492d3714
Stephen Herbein <sherbein@nvidia.com>
2026-03-10 08:43:24 -0700

    [emacs] tweak claude-code-ide
    Switch default width to 150
    Change prefix from C-c C-' (requires OSC codes) to C-c c

--- link/.emacs.d/site-lisp/setup-ai.el
+++ link/.emacs.d/site-lisp/setup-ai.el
@@ -1,11 +1,12 @@
 (use-package claude-code-ide
   :straight (:type git :host github :repo "manzaltu/claude-code-ide.el")
   :bind
-  ("C-c C-'" . claude-code-ide-menu)
+  ("C-c c" . claude-code-ide-menu)
   :config
   (claude-code-ide-emacs-tools-setup)
   :init
   (setq claude-code-ide-terminal-backend 'vterm)
+  (setq claude-code-ide-window-width 150)
   )
 
 (provide 'setup-ai)
