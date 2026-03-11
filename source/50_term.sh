6e9bef22d70dbbca870eae9acdc72be4f75e0b03
Stephen Herbein <sherbein@nvidia.com>
2026-03-11 06:32:26 -0700

    [source/50*] run editor after term; fixup COLORTERM
    set COLORTERM=truecolor whenever we override TERM to vscode-direct

--- source/50_term.sh
+++ source/50_term.sh
@@ -1,3 +1,12 @@
+# Set TERM for emacs
+if [[ -f /usr/share/terminfo/v/vscode-direct ]]; then
+    TERM="vscode-direct"
+    export COLORTERM=truecolor
+else
+    TERM="xterm-emacs"
+fi
+
 if [[ "$TERM" == "xterm-ghostty" ]]; then
     export TERM=xterm-256color
+    export COLORTERM=truecolor
 fi
--- source/51_editor.sh
+++ source/51_editor.sh
@@ -1,11 +1,5 @@
 # Editing
 
-if [[ -f /usr/share/terminfo/v/vscode-direct ]]; then
-    TERM="vscode-direct"
-else
-    TERM="xterm-emacs"
-fi
-
 if type emacsclient &> /dev/null; then
     EDITOR="emacsclient"
     alias emacsclient="TERM=$TERM emacsclient"
