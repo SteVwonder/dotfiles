dc8b2dbeeec7f45069c2e6a8f8e4479ac987520b
Stephen Herbein <sherbein@nvidia.com>
2026-04-28 08:38:54 -0700

    [shell] define a magit function for easy launching

--- source/51_editor.sh
+++ source/51_editor.sh
@@ -9,6 +9,10 @@ else
     alias emacs="TERM=$TERM emacs"
 fi
 
+magit() {
+    MAGIT_REPO="${1:-$PWD}" emacs -t -u -a "" -e '(progn (magit-status (getenv "MAGIT_REPO")) (delete-other-windows))'
+}
+
 export EDITOR="${EDITOR}"
 export SUDO_EDITOR=$EDITOR
 export ALTERNATE_EDITOR=""
