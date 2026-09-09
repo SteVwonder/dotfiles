beab01bf65b7b91988a48241eb2c4eecfa497589
Stephen Herbein <sherbein@nvidia.com>
2026-09-09 18:39:38 +0000

    [emacs] disable flycheck in go for large monorepos

--- link/.emacs.d/site-lisp/setup-flycheck.el
+++ link/.emacs.d/site-lisp/setup-flycheck.el
@@ -1,5 +1,14 @@
+(defun my/disable-flycheck-in-go-buffer ()
+  "Disable Flycheck in Go buffers.
+
+The standard Go checkers do not understand our Bazel monorepo layout well
+enough to provide useful diagnostics."
+  (flycheck-mode -1))
+
 (use-package flycheck
-  :hook (after-init . global-flycheck-mode))
+  :hook ((after-init . global-flycheck-mode)
+         (go-mode . my/disable-flycheck-in-go-buffer)
+         (go-ts-mode . my/disable-flycheck-in-go-buffer)))
 
 (use-package flycheck-irony
   :after flycheck
